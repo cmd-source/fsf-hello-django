ec9bd8d6bccb62892f2eee02a984c7a804b872f2
cmd-source <conorcrosbie09@gmail.com>
2021-08-29 14:22:32 +0000

    adding a new task item view, not yet working

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Item
 # Create your views here.
 
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'item': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    if request.method == "POST":
+        name = request.POST.get("item_name")
+        done = 'done' in request.POST
+        Item.objects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
+    return render(request, 'todo/add.html')
